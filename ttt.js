8921cacce1bba4e917f23b251398a59d21e8b983
Brett Cassedy <brett.cassedy@gmail.com>
2014-06-11 15:45:39 -0700

    Check for winner

--- ttt.js
+++ ttt.js
@@ -26,6 +26,75 @@
     var diagonalPositions1 = [[0, 0], [1, 1], [2, 2]];
     var diagonalPositions2 = [[2, 0], [1, 1], [0, 2]];
 
-    
+    var winner = null;
+
+    _.each(Game.marks, function (mark) {
+      function wonDiagonal (diagonalPositions) {
+        return _.every(diagonalPositions, function (pos) {
+          return game.board[pos[0]][pos[1]] === mark;
+        });
+      }
+
+      var won = _.any(
+        [diagonalPositions1, diagonalPositions2],
+        wonDiagonal
+      );
+
+      if (won) {
+        winner = mark;
+      }
+    });
+
+    return winner;
+  };
+
+  Game.prototype.horizontalWinner = function() {
+    var game = this;
+
+    var winner = null;
+
+    _.each(Game.marks, function (mark) {
+      var indices = [0, 1, 2];
+
+      var won = _.any(indices, function (i) {
+        return _.every(indices, function (j) {
+          return game.board[i][j] === mark;
+        });
+      });
+
+      if (won) {
+        winner = mark;
+      }
+
+    });
+
+    return winner;
+  };
+
+  Game.prototype.verticalWinner = function() {
+    var game = this;
+
+    var winner = null;
+    _.each(Game.marks, function (mark) {
+      var indices = [0, 1, 2];
+
+      var won = _.any(indices, function (j) {
+        return _.every(indices, function (i) {
+          return game.board[i][j] === mark;
+        });
+      });
+
+      if (won) {
+        winner = mark;
+      }
+    });
+
+    return winner;
+  };
+
+  Game.prototype.winner = function() {
+    return (
+      this.diagonalWinner() || this.horizontalWinner() || this.verticalWinner()
+    );
   };
 });
